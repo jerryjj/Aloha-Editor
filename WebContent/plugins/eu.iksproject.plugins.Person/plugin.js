330216c6dbd533416ec392dd59fd0a889e386587
Jerry Jalava <jerry.jalava@gmail.com>
2010-07-27 16:38:32 +0300

    Added simple sidebar support

--- WebContent/plugins/eu.iksproject.plugins.Person/plugin.js
+++ WebContent/plugins/eu.iksproject.plugins.Person/plugin.js
@@ -17,11 +17,32 @@ eu.iksproject.Utils.RDFa.registerNamespaceHandler(
     ['Person'] // Supported classes from the namespace
 );
 
+eu.iksproject.PersonPlugin.activeElement = null;
+
 /**
  * Initialize the plugin, register the buttons
  */
 eu.iksproject.PersonPlugin.init = function() {
 	var that = this;
+	
+	eu.iksproject.PersonPlugin.usedProperties = {
+        'name': {
+            'elementOptions': {visible: true},
+            'form': {
+                'label': GENTICS.Aloha.i18n(eu.iksproject.PersonPlugin, 'property.name'),
+            }
+        },
+        'nickname': {
+            'form': {
+                'label': GENTICS.Aloha.i18n(eu.iksproject.PersonPlugin, 'property.nickname'),
+            }
+        },
+        'url': {
+            'form': {
+                'label': GENTICS.Aloha.i18n(eu.iksproject.PersonPlugin, 'property.url'),
+            }
+        }
+    };
 	
 	this.initButtons();
 	this.initPanel();
@@ -36,25 +57,29 @@ eu.iksproject.PersonPlugin.toString = function() {
 };
 
 eu.iksproject.PersonPlugin.initButtons = function() {
+    var that = this;
+    
+    var personRDFaElement = new eu.iksproject.Utils.RDFa.Element('http://rdf.data-vocabulary.org/#', 'Person');
+    jQuery.each(eu.iksproject.PersonPlugin.usedProperties, function(name, data) {
+        personRDFaElement.addProperty(name, data.elementOptions);
+    });
+    
 	// the 'create person' button
 	this.createPersonButton = new GENTICS.Aloha.ui.Button({
 		'iconClass' : 'GENTICS_button GENTICS_button_addPerson',
 		'size' : 'small',
 		'tooltip' : this.i18n('button.person.tooltip'),
+		'markupWrapper': personRDFaElement.getElement(true),
 		'onclick' : function (element, event) {
 			if (GENTICS.Aloha.activeEditable) {
 				GENTICS.Aloha.activeEditable.obj[0].focus();
 			}
 			
-			var markup = new eu.iksproject.Utils.RDFa.Element('http://rdf.data-vocabulary.org/#', 'Person', {'shortHandle': 'v'});
-			markup.addProperty('name');
-			markup.addProperty('url');
-			
 			var rangeObject = GENTICS.Aloha.Selection.rangeObject;
             
 			// add the markup
-			GENTICS.Utils.Dom.addMarkup(rangeObject, markup.getElement());
-			//GENTICS.Utils.Dom.addMarkup(rangeObject, markup.getProperty('name').getElementTree()); //this is the same thing
+			GENTICS.Utils.Dom.addMarkup(rangeObject, personRDFaElement.getElement());
+			//GENTICS.Utils.Dom.addMarkup(rangeObject, personRDFaElement.getProperty('name').getElementTree()); //this is the same thing
             
 			// select the modified range
 			rangeObject.select();
@@ -68,46 +93,103 @@ eu.iksproject.PersonPlugin.initButtons = function() {
 		this.createPersonButton,
 		this.i18n('tab.annotations'),
 		1
-	);	
+	);
+	
+	// add the event handler for selection change
+	GENTICS.Aloha.EventRegistry.subscribe(GENTICS.Aloha, 'selectionChanged', function(event, rangeObject) {
+		// iterate over all buttons
+		for (var i = 0; i < rangeObject.markupEffectiveAtStart.length; i++) {
+			var effectiveMarkup = rangeObject.markupEffectiveAtStart[i];
+			if (! jQuery(effectiveMarkup).attr('typeof')) {
+			    continue;
+		    }
+		    
+			if (GENTICS.Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, that.createPersonButton.markupWrapper)) {			    
+                that.showPanel(effectiveMarkup);
+                var panelActive = true;
+		    }
+		}
+		
+		if (! panelActive) {
+		    that.hidePanel();
+		}
+	});
 };
 
 eu.iksproject.PersonPlugin.initPanel = function () {
 	var that = this;
-
-	this.panel = new Ext.Window({
-		items : new Ext.FormPanel({
-			labelWidth: 75,
-	        frame:true,
-	        title: 'Simple Form',
-	        bodyStyle:'padding:5px 5px 0',
-	        width: 350,
-	        defaults: {width: 230},
-	        defaultType: 'textfield',
-	        items: [{
-                fieldLabel: 'Name',
-                name: 'name',
-            },{
-                fieldLabel: 'Nickname',
-                name: 'nickname'
-            },{
-                fieldLabel: 'URL',
-                name: 'url'
-            }
-        ]
-		}),
-		buttons: [{
-            text:'Submit',
-            handler : function () {
-				
-			}
-        },{
-            text: 'Close',
-            handler: function(){
-                that.panel.hide();
-            }
-        }]
+    
+    var form_items = [];
+    var item = null;    
+    jQuery.each(eu.iksproject.PersonPlugin.usedProperties, function(name, data) {
+        edit_item = {
+            name: name,
+            fieldLabel: data.form.label
+        };
+        form_items.push(edit_item);
+        
+        visibility_item = {
+            name: name + '_visibility',
+            fieldLabel: that.i18n('form.visibility'),
+            xtype: 'checkbox'
+        };
+        form_items.push(visibility_item);
+    });
+    
+    this.propertyFormPanel = new Ext.FormPanel({
+		labelWidth: 75,
+        frame: true,
+        title: this.i18n('button.person.tooltip'),
+        bodyStyle: 'padding:5px 5px 0',
+        width: 350,
+        defaults: {width: 230},
+        defaultType: 'textfield',
+        items: form_items
 	});
+    
+    var submit = this.propertyFormPanel.addButton({
+            text: 'Save',
+            handler: function(){
+                var rdfaEl = eu.iksproject.PersonPlugin.activeElement;
+                var form = that.propertyFormPanel.getForm();
+                var opts = {};
+                            
+                jQuery.each(rdfaEl.properties, function(name, property) {
+                    opts = {
+                        visible: form.findField(name + '_visibility').getValue()
+                    };
+                    eu.iksproject.PersonPlugin.activeElement.getProperty(name).update(form.findField(name).getValue(), opts);
+                });
+            }
+        });
+    
+    this.panel = new Ext.Window({
+        resizable: false,
+        items : this.propertyFormPanel
+    });
 
     this.panel.render(document.body);
+};
+
+eu.iksproject.PersonPlugin.showPanel = function(effectiveMarkup) {
+    var personRDFaElement = eu.iksproject.Utils.RDFa.readElementFrom(effectiveMarkup);
+    
+    var form = this.propertyFormPanel.getForm();
+    
+    jQuery.each(personRDFaElement.properties, function(name, property) {
+        if (form.findField(name)) {
+            form.findField(name).setValue(property.value);
+            form.findField(name + '_visibility').setValue(property.options.visible);            
+        }
+    });
+    
+    eu.iksproject.PersonPlugin.activeElement = personRDFaElement;
+    
     this.panel.show();
+};
+
+eu.iksproject.PersonPlugin.hidePanel = function() {
+    eu.iksproject.PersonPlugin.activeElement = null;
+    
+    this.panel.hide();
 };
